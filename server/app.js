e667910744dce500ed9d3271063b639675290ac4
anggraito <anggraito.laras@gmail.com>
2017-09-20 17:24:36 +0700

    seeting app.js di /user menjadi /

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-app.use('/user', user)
+app.use('/', user)
 
 app.listen(port);
 console.log('Your app is running on http://localhost:' + port);
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,1 +1,16 @@
 const modelUser = require('../models/User')
+
+var getAllUser = (req, res) => {
+  modelUser.find()
+  .then((users) => {
+    res.send(users)
+  })
+  .catch((err) => {
+    res.send(err)
+  })
+}
+
+
+module.exports = {
+  getAllUser
+}
