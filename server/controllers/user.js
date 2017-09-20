976d220170af4054e84be447d19fe8fad8a3bf69
anggraito <anggraito.laras@gmail.com>
2017-09-20 17:46:36 +0700

    add file helpers

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,8 @@
 const modelUser = require('../models/User')
+var jwt = require('jsonwebtoken')
+var bcrypt = require('bcryptjs')
+var salt = bcrypt.genSaltSync(10);
+require('dotenv').config()
 
 var getAllUser = (req, res) => {
   modelUser.find()
@@ -10,7 +14,48 @@ var getAllUser = (req, res) => {
   })
 }
 
+var signUp = (req, res) => {
+  let hashPassword = bcrypt.hashSync(req.body.password, salt);
+  modelUser.create({
+    username: req.body.username,
+    password: hashPassword,
+    email: req.body.email
+  })
+  .then((user) => {
+    res.send({
+      message: "Berhasil menambah user",
+      user: user
+    })
+  })
+  .catch(err => {
+    res.send(err)
+  })
+}
+
+var sigIn = (req, res) => {
+  modelUser.findOne({
+    username: req.body.username
+  })
+  .then((user) => {
+    var decodePassword = bcrypt.compareSync(req.body.password, user.password)
+    if(decodePassword) {
+      var token = jwt.sign({
+        id: user._id,
+        username: user.username,
+        email: user.email
+      }, process.env.SECRET_JWT)
+      res.send(token)
+    } else {
+      res.send({
+        message: "Password tidak cocok"
+      })
+    }
+  })
+  .catch(err => {
+    res.send(err)
+  })
+}
 
 module.exports = {
-  getAllUser
+  getAllUser, signUp, sigIn
 }
