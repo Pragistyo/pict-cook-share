d76b4a2db292d610d2377e36deaccb842ce7c834
anggraito <anggraito.laras@gmail.com>
2017-09-20 17:13:26 +0700

    skeleton app

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express'),
+      morgan = require('morgan'),
+      bodyParser = require('body-parser'),
+      cors = require('cors'),
+      mongoose = require('mongoose');
+const port = process.env.PORT || 3000
+const app = express()
+
+var url = 'mongodb://anggraito:<PASSWORD>@mydb-shard-00-00-vv98n.mongodb.net:27017,mydb-shard-00-01-vv98n.mongodb.net:27017,mydb-shard-00-02-vv98n.mongodb.net:27017/recipe?ssl=true&replicaSet=mydb-shard-0&authSource=admin';
+mongoose.connect(url);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function() {
+   console.log(`You're connected in this database`)
+});
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+app.listen(port);
+console.log('Your app is running on http://localhost:' + port);
