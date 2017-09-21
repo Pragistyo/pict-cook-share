8ebccf18b2b8fb44cf7d0dff384b5b5624aed1b8
anggraito laras <anggraito.laras@gmail.com>
2017-09-22 01:02:08 +0700

    delete sign.js in routes
    duplicate file, sudah ada di user.js routes

--- server/routes/sign.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const userController = require('../controllers/user')
-
-router.post('/signup', userController.signUp)
-router.post('/signin', userController.signIn)
-
-module.exports = router
