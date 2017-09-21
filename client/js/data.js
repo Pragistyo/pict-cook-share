57f297ff28c6b43971c342550c3e9d4bbdaaafb6
anggraito <anggraito.laras@gmail.com>
2017-09-21 22:57:58 +0700

    axios output data food fork recipe

--- client/js/data.js
+++ client/js/data.js
@@ -1,71 +1,93 @@
-$(document).ready(function() {
-  console.log('Document is Ready')
+$('form#kirimGambar').on('submit', (event) => {
+  event.preventDefault()
+  var data = new FormData();
+  data.append('filePhoto', $('#filePhoto')[0].files[0])
+
+  axios({
+    method: 'POST',
+    url: 'http://localhost:3000/vision',
+    data: $('#filePhoto')[0].files[0],
+    processData: false
+  })
+  .then((data)=>{
+    console.log(data)
+    //nanti return axios food disini
+  })
+  .catch(err => {
+    console.log(err)
+  })
 })
 
-  var $kirimGambar = $('#kirimGambar')
-  $('form.kirimGambar').submit(function(eventHandler){
-    eventHandler.preventDefault();
-    axios.post('http://localhost:3000/vision', {
-      gambar : $kirimGambar.val()
-    })
-    .then(response => {
-      console.log(response);
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  })
-
-  var search = $('input[name="search"]')
-  seacrh.keyup(function() {
-    let keyword = $(this).val()
-    $.ajax({
-      url: `https://community-food2fork.p.mashape.com/search?key=3f0eb392931eb697ef53ea1cf86478f8&q=${keyword}`,
-      type: 'GET',
-      dataType: 'json',
-      success: function(response){
-        console.log(response)
-      },
-      fail: function(err) {
-        console.log(err)
+var search = $('input[name="search"]')
+search.keyup(function() {
+  let keyword = $(this).val()
+  $.ajax({
+    url: `https://community-food2fork.p.mashape.com/search?key=3f0eb392931eb697ef53ea1cf86478f8&page=1&q=${keyword}`,
+    type: 'GET',
+    headers: {
+      'X-Mashape-Key': 'ft6JTtlfammshF6NyVGY4XGOwdxnp1VENsbjsnlbs3PxWiJZdd',
+      'Accept': 'application/json'
+    },
+    dataType: 'json',
+    success: function(response){
+      console.log(response)
+      $('h3.count-recipe').empty()
+      $('div.content-recipe').empty()
+      $('h3.count-recipe').append(`${response.count} from ${keyword}`)
+      var result = response.recipes 
+      for(var i=0; i < result.length; i++){
+        $('div.content-recipe').append(`
+          <div class="col-sm-6 col-md-4" onclick="recipe(${result[i].recipe_id})">
+            <div class="thumbnail">
+              <div class="images" style="background-image: url('${result[i].image_url}')">
+              </div>
+              <div class="caption">
+                <h4>${result[i].title}</h4>
+                <p>Publisher: <a href="${result[i].publisher_url}">${result[i].publisher}</a></p>
+              </div>
+            </div>
+          </div>  
+        `)
       }
-    })
+    },
+    fail: function(err) {
+      console.log(err)
+    }
   })
+})
 
-  // $.ajax({
-  //   type: 'GET',
-  //   url: `localhost:3000/vision`,
-  //   dataType: 'json',
-  //   success: function(data) {
-  //     console.log(data)
-  //   },
-  //   error: function(error) {
-  //     console.log(error)
-  //   }
-  // })
-
-  // $.ajax({
-  //   type: 'GET',
-  //   url: `https://community-food2fork.p.mashape.com/search?key=3f0eb392931eb697ef53ea1cf86478f8&q=shredded+chicken`,
-  //   dataType: 'json',
-  //   success: function(data) {
-  //     console.log(data)
-  //   },
-  //   error: function(error) {
-  //     console.log(error)
-  //   }
-  // })
-
-  // $.ajax({
-  //   type: 'GET',
-  //   url: `https://community-food2fork.p.mashape.com/get?key=3f0eb392931eb697ef53ea1cf86478f8&rId=37859`,
-  //   dataType: 'json',
-  //   success: function(data) {
-  //     console.log(data)
-  //   },
-  //   error: function(error) {
-  //     console.log(error)
-  //   }
-  // })
-
-
+function recipe(id){
+  $('h3.count-recipe').empty()
+  $('div.content-recipe').empty()
+  $.ajax({
+    url: `https://community-food2fork.p.mashape.com/get?key=3f0eb392931eb697ef53ea1cf86478f8&rId=${id}`,
+    type: 'GET',
+    headers: {
+     'X-Mashape-Key': 'ft6JTtlfammshF6NyVGY4XGOwdxnp1VENsbjsnlbs3PxWiJZdd',
+     'Accept': 'application/json'
+    },
+    dataType: 'json',
+    success: function(data){ 
+      console.log(data.recipe)
+      $('div.content-recipe').append(`
+      <div class="col-md-4 thumbnail">
+        <div class="images"></div>
+        <p class="rating"><span>Rating</span></p>
+      </div>
+      <div class="col-md-8 detail-item">
+        <h3>Judul resep masakannya</h3>
+        <p>Publisher</p>
+        <ul class="ingredient-wrap">
+          <li>
+            <span class="glyphicon glyphicon glyphicon-cutlery" aria-hidden="true"></span>
+            Ingredient
+          </li>
+        </ul>
+      </div>
+      `)
+    },
+    fail: function(err){
+      console.log(err);
+    }
+  })
+}
